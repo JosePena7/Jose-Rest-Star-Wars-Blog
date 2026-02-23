2c8ea85adc22cd621db03a64a4e5a1f3e32cff48
JosePena7 <Josesworkday23@icloud.com>
2026-02-23 23:06:30 +0000

    StarwarsBlog

--- src/models.py
+++ src/models.py
@@ -1,19 +1,121 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Integer, ForeignKey 
+from sqlalchemy.orm import Mapped, mapped_column,relationship
+from typing import List
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    favorites : Mapped[List["Favorite"]] = relationship("Favorite", back_populates ="user")
+    posts: Mapped[List["Post"]] = relationship("Post", back_populates = "author")
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+            #
+            #
+            #
+
+        }
+
+
+class Favorite(db.Model):
+    __tablename__ = "favorite"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    character_id: Mapped[int] = mapped_column(
+        ForeignKey("character.id"), nullable=True)
+    planet_id: Mapped[int] = mapped_column(
+        ForeignKey("planet.id"), nullable=True)
+
+    user: Mapped["User"] = relationship("User", back_populates="favorites")
+    character: Mapped["Character"] = relationship(
+        "Character", back_populates="favorites")
+    planet: Mapped["Planet"] = relationship(
+        "Planet", back_populates="favorites")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "character_id": self.character_id,
+            "planet_id": self.planet_id,
+            
+        }
+
+
+class Character(db.Model):
+    __tablename__ = "character"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    gender: Mapped[str] = mapped_column(String(50))
+    height: Mapped[str] = mapped_column(String(50))
+    eye_color: Mapped[str] = mapped_column(String(50))
+
+    favorites: Mapped[List["Favorite"]] = relationship(
+        "Favorite", back_populates="character")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "height": self.height,
+            "eye_color": self.eye_color
+        }
+class Planet(db.Model):
+    __tablename__ ="planet"
+
+    id: Mapped[int] = mapped_column(primary_key = True)
+    name: Mapped[str] = mapped_column(String(150), nullable = False)
+    climate:Mapped[str]= mapped_column (String(120))
+    population: Mapped[str] = mapped_column(String(120))
+    terrain: Mapped[str] = mapped_column (String(120))
+
+
+    favorites: Mapped[List["Favorite"]] = relationship("Favorite", back_populates= "planet")
+
+
+    def serialize(self):
+
+        return{
+            "id": self.id,
+            "name":self.name,
+            "climate":self.climate,
+            "population":self.population,
+            "terrain": self.terrain
+
+
+        }
+    
+class Post(db.Model):
+    __tablename__ = "post"
+
+
+    id: Mapped[int] = mapped_column(primary_key = True)  
+    title: Mapped[str] = mapped_column( String(120), nullable = False)
+    content: Mapped[str] = mapped_column(String(500), nullable=False)
+    user_id: Mapped[int] = mapped_column (ForeignKey("user.id"), nullable =False) 
+
+    author: Mapped["User"] = relationship("User", back_populates ="posts")
+ 
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "title": self.title,
+            "content": self.content,
+            "user_id": self.user_id
         }
